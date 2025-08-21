eb04d84a861d144002e3f3cdb42d1a6c53cec50b
Gaetan Fabo <156204045+Gaetanfabo@users.noreply.github.com>
2025-08-21 21:52:31 +0200

    app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';  
+import { HomeComponent } from './home/home.component';
+import { AProposComponent } from './a-propos/a-propos.component';
+import { NousContacterComponent } from './nous-contacter/nous-contacter.component';
+import { TelephoneComponent } from './telephone/telephone.component'; 
+import { ServeurComponent } from './serveur/serveur.component';
+import { EquipReseauComponent } from './equip-reseau/equip-reseau.component';
+import { AccessoireComponent } from './accessoire/accessoire.component';
+import { ImprimanteComponent } from './imprimante/imprimante.component';
+import { OrdinateursBureauxComponent } from './ordinateurs-bureaux/ordinateurs-bureaux.component';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common'; 
 import { ServicesComponent } from './services/services.component';
 import { CataloguesComponent } from './catalogues/catalogues.component';
 import { OrdinateursPortablesComponent } from './ordinateurs-portables/ordinateurs-portables.component';
 
 const routes: Routes = [
-        { path: '', component: AppComponent },
-        { path: 'catalogue', component: CataloguesComponent },
-        { path: 'service', component: ServicesComponent },
-        { path: 'ordinateurs-portables', component: OrdinateursPortablesComponent }, // Example route for laptops
-          
+      {path: 'telephone', component: TelephoneComponent},
+          {path: 'serveur', component: ServeurComponent},
+          {path: 'equip-reseau', component: EquipReseauComponent},
+          {path: 'accessoire', component: AccessoireComponent},
+          {path: 'imprimante', component: ImprimanteComponent},
+          {path: 'ordinateurs-bureaux', component: OrdinateursBureauxComponent},
+          {path: 'ordinateurs-portables', component: OrdinateursPortablesComponent},
+          {path: 'a-propos', component: AProposComponent},
+          {path: 'nous-contacter', component: NousContacterComponent},
+          {path:'services',component: ServicesComponent},
+          {path: 'catalogues', component: CataloguesComponent},
+          {path: '', component: HomeComponent},
 ]; 
 
 @NgModule({
