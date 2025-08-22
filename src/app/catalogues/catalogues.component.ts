283cadf573ffcd3ca96d2eb06136374b79edcd77
Gaetan Fabo <156204045+Gaetanfabo@users.noreply.github.com>
2025-08-22 16:58:57 +0200

    Fix prerendering errors by removing invalid HTML structure

--- src/app/catalogues/catalogues.component.ts
+++ src/app/catalogues/catalogues.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-catalogues',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './catalogues.component.html',
   styleUrl: './catalogues.component.scss'
 })
@@ -13,8 +14,8 @@ export class CataloguesComponent {
   allerVersContact() {
     this.router.navigate(['/ordinateurs-portables']);
     this.router.navigate(['/ordinateurs-bureaux']);
-    this.router.navigate(['/imprimantes']); 
-    this.router.navigate(['/accessoires']);
+    this.router.navigate(['/imprimante']); 
+    this.router.navigate(['/accessoire']);
     this.router.navigate(['/equip-reseau']);
     this.router.navigate(['/serveur']);
     this.router.navigate(['/telephone']);
